75cbe400074a1fc006010ef2f02b7fd55c345616
Margaro Sanchez <sichibukay@icloud.com>
2019-01-24 11:55:03 -0600

    added cors

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const frutasRouter = require('./routes/frutas');
 
 app.use(bodyParser.urlencoded({extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/',function(req, res){
   res.json({message: 'Bienvenido al store Proteco'});
